3e44569b73f7a8938e6bc9e833c38d363636b3b3
rodolpho_maia <rodolpho.java@gmail.com>
2020-02-08 13:52:37 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const routes = Router();
 routes.post("/newuser", UserController.store);
 routes.get("/users", UserController.index);
 routes.post("/login", UserController.login);
+routes.post("/forgotpassword", UserController.forgotPassword);
 
 module.exports = routes;
